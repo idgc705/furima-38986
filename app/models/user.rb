c457dbae378a5a16aba9c27b5308884ba809a99c
idgc705 <idgc705@gmail.com>
2023-06-30 19:09:55 +0900

    ユーザー管理修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
 
          
          validates :nickname, presence: true
-         validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
          validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday, presence: true
-         validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 end
