823aed43006bc7e79e31a4752a9fc8d1a0886419
idgc705 <idgc705@gmail.com>
2023-07-13 21:55:43 +0900

    factoryの編集

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :order_address do
-    postcode { '123-4567' }
+    token         {'TOKENtoken'}
+    postcode      { '123-4567' }
     prefecture_id { '1' }
-    city { '市区町村' }
-    block { '1-1' }
-    building { 'ビル' }
-    phone_number { '0123456789' }
+    city          { '市区町村' }
+    block         { '1-1' }
+    building      { 'ビル' }
+    phone_number  { '0123456789' }
   end
 end
