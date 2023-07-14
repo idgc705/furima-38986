472dd140607c1b0bab7dc0c19a5c538db0c92da1
idgc705 <idgc705@gmail.com>
2023-07-14 19:19:19 +0900

    購入機能の修正2

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :order_id, :token
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :token
 
   with_options presence: true do
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Postcode is invalid. Include hyphen(-)' }
